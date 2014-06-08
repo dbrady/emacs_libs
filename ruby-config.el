a8cea407bd4ce8f200db4700a7b941570de0f974
David Brady <dbrady@shinybit.com>
2014-06-08 13:27:46 -0600

    Add TODO wishlist for check-uncheck-commented-item
    * E.g. In ruby if I build an org-mode-like checklist, allow C-c C-c to
      check and uncheck it

--- ruby-config.el
+++ ruby-config.el
@@ -1,4 +1,3 @@
-
 ;; ;; Rinari
 ;; (add-to-list 'load-path "~/emacs_libs/rinari")
 ;; (require 'rinari)
@@ -39,7 +38,7 @@
 (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
 
 
-					; Install mode-compile to give friendlier compiling support!
+          ; Install mode-compile to give friendlier compiling support!
 (autoload 'mode-compile "mode-compile"
   "Command to compile current buffer file based on the major mode" t)
 (global-set-key "\C-ck" 'mode-compile)
@@ -71,19 +70,51 @@
 
 (dolist (command '(yank yank-pop))
   (eval `(defadvice ,command (after indent-region activate)
-	   (and (not current-prefix-arg)
-		(member major-mode '(emacs-lisp-mode lisp-mode
-						     clojure-mode    scheme-mode
-						     haskell-mode    ruby-mode
-						     rspec-mode      python-mode
-						     c-mode          c++-mode
-						     objc-mode       latex-mode
-						     plain-tex-mode))
-		(let ((mark-even-if-inactive transient-mark-mode))
-		  (indent-region (region-beginning) (region-end) nil))))))
+     (and (not current-prefix-arg)
+    (member major-mode '(emacs-lisp-mode lisp-mode
+                 clojure-mode    scheme-mode
+                 haskell-mode    ruby-mode
+                 rspec-mode      python-mode
+                 c-mode          c++-mode
+                 objc-mode       latex-mode
+                 plain-tex-mode))
+    (let ((mark-even-if-inactive transient-mark-mode))
+      (indent-region (region-beginning) (region-end) nil))))))
+
+
+;; ---------------------------------------------------------------------
+;; check-uncheck-commented-item
+;;
+;; If the current line begins with \s*#\s*-\s*[(\s|X)], toggle the
+;; square brackets between [ ] and [X]. Bind to C-c C-c in the current
+;; mode (beware, this may conflict with compile in many programming
+;; modes)
+;;
+;; TODO: Extra bonus coolif: search upwards in the same comment block
+;; for a line like # * Some Title [(\d*)/(\d*)] and update the \d's.
+;;
+;; TODO: For now only worry about ruby-mode, but ideally this could
+;; work with any mode that has a comment-start string (and, if we
+;; really want to be pedantic, one that has a comment-end string, so
+;; we could do it in HTML and maybe C if we didn't want to use the
+;; common idiom of /* at the top of the block, * on middling lines,
+;; and */ on the final line)
+;;
+;; TODO: How do I make this ONLY run in ruby-mode? Probably by binding
+;; C-c C-c to this function but only in a ruby-mode
+;;
+;; TODO: Possibly worth investigating: I hear MuMaMo is old and
+;; busted, but mayhap there is a way to
+;; (defun check-uncheck-commented-item ()
+;;   (interactive)
+;;   (save-excursion
+;;     ;; KWM: Write me! Not sure how. Perhaps narrow-to-current-line,
+;;     ;; beginning-of-line, then try a replace-regexp on [( |X)] that
+;;     ;; replaces the match with the opposite of whatever it found?
+;;     ))
 
 (add-hook 'ruby-mode-hook '(lambda ()
-			     (local-set-key (kbd "RET") 'reindent-then-newline-and-indent))) ;newline-and-indent)))
+           (local-set-key (kbd "RET") 'reindent-then-newline-and-indent))) ;newline-and-indent)))
 
 (require 'flymake-ruby)
 (require 'rspec-mode)
