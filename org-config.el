1293875377a8986a9328204fc4a950a6105a90fd
David Brady <dbrady@shinybit.com>
2014-06-08 13:28:19 -0600

    Add journal and org-mode convenience methods
    * org-uncheck-everything resets all org-mode checklists back to
      unchecked and pristine state, including their * TODO [n/N] header
      counts
    
    * org-ctrl-c-ctrl-c-and-next-line toggles the current item and moves
      down, useful for checking off things en masse (needs work; C-x c is
      actually more work than C-(ccn). Need to tweak this, especially with
      moving to the next task item, not just the next line (for multiline
      tasks)
    
    * org-insert-journal-title inserts a standardized journal title
      string, but don't use it except in the rare case when you're in a
      brand-new journal document. If you are editing an existing journal,
      this method is called for you as part of
    
    * org-save-journal-as-today updates the journal title with today's
      date, then saves the buffer with the name YYYY-mm-dd-todo.org, where
      YYYY-mm-dd is the current date

--- org-config.el
+++ org-config.el
@@ -42,6 +42,45 @@
 ;; ----------------------------------------------------------------------
 
 ;; ----------------------------------------------------------------------
+;; org-ctrl-c-ctrl-c-and-next-line
+;;
+;; TODO: next-line until org-at-item-p would be great, it would move
+;; me to the next checkbox---UNLESS there are no more items. But how
+;; do I tell? ALSO, don't stop on a checkbox if it has children--move
+;; to the first childless descendant, that way I can keep checking
+;; things off as I go down.
+;;
+;; NOTE: PDI: When I wrote org-uncheck-everything, I got around all
+;; this semantic mess by going to the END of the document and working
+;; up. This has the obvious drawback of working only in documents that
+;; have exactly one checklist (or this is a feature where "everything"
+;; means "EVERYTHING"). However, I recently dug into the org-mode
+;; source code and found org-at-item-p, which makes me wonder if there
+;; aren't methods like org-item-has-children-p or something
+;; similar--I.e. how does org-mode *itself* know there are children,
+;; and when it has reached the end of a checklist and should stop
+;; counting? TODO: research and interface with this API.
+;;
+;; TODO: have this work like kmacro-end-and-call-macro, where you hit
+;; C-x e to execute the macro, then the minibuffer says "Hit e to
+;; execute again". So I'd hit say C-x c, but could then just hit c c c
+;; c to continue checking items off. Also C-u n prefix would be nice,
+;; and C-x 0 would essentially be "check everything off until end of
+;; document". Care would have to be taken to skip over items that have
+;; children. See previous note about org-mode API research; ideally
+;; the C-u 0 version should stop at the end of the current checklist
+;; if there is more than one checklist in the document.
+; ----------------------------------------------------------------------
+(defun org-ctrl-c-ctrl-c-and-next-line ()
+  (interactive)
+  (when (org-at-item-p)
+    (org-ctrl-c-ctrl-c)
+    (next-line)))
+
+(add-hook 'org-mode-hook
+          (lambda () (local-set-key (kbd "C-x c") #'org-ctrl-c-ctrl-c-and-next-line)))
+
+;; ----------------------------------------------------------------------
 ;; KWM: org-insert-journal-title
 
 ;; insert e.g. "* 2014-05-09 Fri TODO\n\n\n" at the top of the
@@ -49,17 +88,45 @@
 ;; day's journal, so ideally also search for the existence of "*
 ;; \d\d\d\d-\d\d-\d\d .* TODO" at the top of the document, and if
 ;; found, delete that line first.
+;;
+;; Thought: a better defun here might actually be
+;; save-journal-as-today or similar, that updates the string, and also
+;; writes the file to the new name of YYYY-MM-DD-todo.org.
 ;; ----------------------------------------------------------------------
+(defun replace-first-line-with (string)
+  (save-excursion
+    (beginning-of-buffer)
+    (kill-line)
+    (insert string)))
 
+(defun org-insert-journal-title ()
+  (interactive)
+  (replace-first-line-with (format-time-string "* %F %a TODO")))
+
+(defun org-save-journal-as-today ()
+  (interactive)
+  (org-insert-journal-title)
+  (write-file (format-time-string "%F-todo.org")))
 
 ;; ----------------------------------------------------------------------
 ;; KWM: org-uncheck-everything
 ;;
-;; PDI. This is probably just a macro or a search-and-replace. Ideally
-;; search for each /^[[:space:]*]- \[X\]/ entry, then C-e, then C-c
-;; C-c.
-;; C-e is move-end-of-line
-;; C-c C-c is org-ctrl-c-ctrl-c
+;; Note that direction matters: org-ctrl-c-ctrl-c will raise an error
+;; (and exit the while loop and the defun) if you try to uncheck an
+;; item that has checked subitems. When doing this manually I either
+;; ignore these errors (the defun could do this by catching the error)
+;; or I check the next line for subitems (the defun could do this by
+;; scanning ahead). However, by walking backwards up the document from
+;; the bottom, all of this can be avoided, since we now uncheck all
+;; child items before reaching the parent item.
 ;; ----------------------------------------------------------------------
+(defun org-uncheck-everything ()
+  (interactive)
+  (save-excursion
+    (end-of-buffer)
+    (while (search-forward "[X]" nil t -1)
+      (move-end-of-line 1)
+      (org-ctrl-c-ctrl-c))))
+
 
 (provide 'org-config)
